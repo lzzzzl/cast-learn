833f956065418741c8474dc2556952b5591decf4
lzzzzl <597023590@qq.com>
2023-02-20 19:48:58 +0800

    feat: updating ToStringSliceE to support some other slices

--- caste.go
+++ caste.go
@@ -1078,16 +1078,46 @@ func ToStringSliceE(i interface{}) ([]string, error) {
 	var a []string
 
 	switch v := i.(type) {
-	case []string:
-		return v, nil
-	case string:
-		return strings.Fields(v), nil
 	case []interface{}:
 		for _, u := range v {
 			s, _ := ToStringE(u)
 			a = append(a, s)
 		}
 		return a, nil
+	case []string:
+		return v, nil
+	case []int8:
+		for _, u := range v {
+			a = append(a, ToString(u))
+		}
+		return a, nil
+	case []int:
+		for _, u := range v {
+			a = append(a, ToString(u))
+		}
+		return a, nil
+	case []int32:
+		for _, u := range v {
+			a = append(a, ToString(u))
+		}
+		return a, nil
+	case []int64:
+		for _, u := range v {
+			a = append(a, ToString(u))
+		}
+		return a, nil
+	case []float32:
+		for _, u := range v {
+			a = append(a, ToString(u))
+		}
+		return a, nil
+	case []float64:
+		for _, u := range v {
+			a = append(a, ToString(u))
+		}
+		return a, nil
+	case string:
+		return strings.Fields(v), nil
 	case interface{}:
 		str, err := ToStringE(v)
 		if err != nil {
