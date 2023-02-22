1aa55018cc8e87dd646f6612ef0e5bde64740866
lzzzzl <597023590@qq.com>
2023-02-22 13:42:45 +0800

    Add ToTimeInDefaultLocation/E

--- caste.go
+++ caste.go
@@ -15,13 +15,20 @@ var errNegativeNotAllowed = errors.New("unable to cast negative value")
 
 // ToTimeE casts an interface to a time.Time type.
 func ToTimeE(i interface{}) (time.Time, error) {
+	return ToTimeInDefaultLocationE(i, time.UTC)
+}
+
+// ToTimeInDefaultLocationE casts an empty interface to time.Time,
+// interpreting inputs without a timezone to be in the given location,
+// or the local timezone if nil.
+func ToTimeInDefaultLocationE(i interface{}, location *time.Location) (tim time.Time, err error) {
 	i = indirect(i)
 
 	switch v := i.(type) {
 	case time.Time:
 		return v, nil
 	case string:
-		return StringToDate(v)
+		return StringToDateInDefaultLocation(v, location)
 	case int:
 		return time.Unix(int64(v), 0), nil
 	case int64:
@@ -1196,37 +1203,66 @@ func ToDurationSliceE(i interface{}) ([]time.Duration, error) {
 // predefined list of formats.  If no suitable format is found, an error is
 // returned.
 func StringToDate(s string) (time.Time, error) {
-	return parseDate(s, []string{
-		time.RFC3339,
-		"2006-01-02T15:04:05", // iso8601 without timezone
-		time.RFC1123Z,
-		time.RFC1123,
-		time.RFC822Z,
-		time.RFC822,
-		time.RFC850,
-		time.ANSIC,
-		time.UnixDate,
-		time.RubyDate,
-		"2006-01-02T15:04:05-0700", // RFC3339 without timezone hh:mm colon
-		"2006-01-02 15:04:05.999999999 -0700 MST", // Time.String()
-		"2006-01-02",
-		"02 Jan 2006",
-		"2006-01-02 15:04:05 -07:00",
-		"2006-01-02 15:04:05 -0700",
-		"2006-01-02 15:04:05Z07:00", // RFC3339 without T
-		"2006-01-02 15:04:05Z0700",  // RFC3339 without T or timezone hh:mm colon
-		"2006-01-02 15:04:05",
-		time.Kitchen,
-		time.Stamp,
-		time.StampMilli,
-		time.StampMicro,
-		time.StampNano,
-	})
+	return StringToDateInDefaultLocation(s, time.UTC)
 }
 
-func parseDate(s string, dates []string) (d time.Time, err error) {
-	for _, dateType := range dates {
-		if d, err = time.Parse(dateType, s); err == nil {
+// StringToDateInDefaultLocation casts an empty interface to a time.Time,
+// interpreting inputs without a timezone to be in the given location,
+// or the local timezone if nil.
+func StringToDateInDefaultLocation(a string, location *time.Location) (time.Time, error) {
+	if location == nil {
+		location = time.Local
+	}
+	return parseDateWith(a, location, timeFormats)
+}
+
+type timeFormat struct {
+	format      string
+	hasTimezone bool
+}
+
+var (
+	timeFormats = []timeFormat{
+		{time.RFC3339, true},
+		{"2006-01-02T15:04:05", false}, // iso8601 without timezone
+		{time.RFC1123Z, true},
+		{time.RFC1123, false},
+		{time.RFC822Z, true},
+		{time.RFC822, false},
+
+		{time.RFC850, true},
+		{"2006-01-02 15:04:05.999999999 -0700 MST", true}, // Time.String()
+		{"2006-01-02T15:04:05-0700", true},                // RFC3339 without timezone hh:mm colon
+		{"2006-01-02 15:04:05Z0700", true},                // RFC3339 without T or timezone hh:mm colon
+		{"2006-01-02 15:04:05", false},
+
+		{time.ANSIC, false},
+		{time.UnixDate, false},
+		{time.RubyDate, true},
+		{"2006-01-02 15:04:05Z07:00", true},
+		{"2006-01-02", false},
+		{"02 Jan 2006", false},
+		{"2006-01-02 15:04:05 -07:00", true},
+		{"2006-01-02 15:04:05 -0700", true},
+		{time.Kitchen, false},
+		{time.Stamp, false},
+		{time.StampMilli, false},
+		{time.StampMicro, false},
+		{time.StampNano, false},
+	}
+)
+
+func parseDateWith(s string, defaultLocation *time.Location, formats []timeFormat) (d time.Time, e error) {
+	for _, format := range formats {
+		if d, e = time.Parse(format.format, s); e == nil {
+			// Some time formats have a zone name, but no offset, so it gets
+			// put in that zone name (not the default one passed in to us), but
+			// without that zone's offset. So set the location manually.
+			if !format.hasTimezone && defaultLocation != nil {
+				year, month, day := d.Date()
+				hour, min, sec := d.Clock()
+				d = time.Date(year, month, day, hour, min, sec, d.Nanosecond(), defaultLocation)
+			}
 			return
 		}
 	}
